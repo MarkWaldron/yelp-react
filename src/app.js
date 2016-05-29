9a364da9f99c54e9929de440b8ce11279eca527a
MarkWaldron <markewaldron@gmail.com>
2016-05-28 22:38:28 -0400

    Added font-awesome and removed unneeded css

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './app.css'
+
+import 'font-awesome/css/font-awesome.css'
+
 import styles from './styles.module.css'
+
 
 const App = React.createClass({
   render: function() {
     return (
-      <div className={styles.wrapper}>
-        <h1>Environment: {__NODE_ENV__}</h1>
+      <div>
+        <h1><i className="fa fa-star"></i>Environment: {__NODE_ENV__}</h1>
       </div>
     )
   }
